89983db576e201d410a37a2aa5061348fe6bdb3f
Svitlana Hrytskiv <91554698+Sv9Kl9@users.noreply.github.com>
2022-12-02 16:07:26 +0200

    with loadVideo

--- js.js
+++ js.js
@@ -20,3 +20,13 @@ Promise.all([player1.ready, player2.ready]).then(() => {
   player1.play();
   player2.play();
 });
+
+
+// Promise.all([player1.loadVideo('https://player.vimeo.com/video/772410430?h=dfaaee220c'), player2.loadVideo('https://player.vimeo.com/video/772410499?h=dc0b01c09d')])
+// .then(()=>{
+//   player1.play();
+//   player2.play();
+// })
+
+
+
